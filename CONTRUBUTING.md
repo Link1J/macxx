929c6d093deecac8eea898391846b178788be175
Jared Irwin <jrairwin@sympatico.ca>
2021-03-13 20:45:09 -0500

    Update Copyright

--- CONTRUBUTING.md
+++ CONTRUBUTING.md
@@ -10,7 +10,7 @@ This is so that we can dissuse the idea.
 > If you add or update a file, put at the very top
 > ```cpp
 > 
-> // Copyright Jared Irwin 2020
+> // Copyright Jared Irwin 2020-2021
 > // Distributed under the Boost Software License, Version 1.0.
 > // (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 > 
--- include/Foundation/Foundation.hpp
+++ include/Foundation/Foundation.hpp
@@ -1,10 +1,11 @@
 
-// Copyright Jared Irwin 2020
+// Copyright Jared Irwin 2020-2021
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
 
 #include "NSObject.functions.hpp"
-#include "NSProcessInfo.functions.hpp"
+#include "NSEnumerator.functions.hpp"
 #include "NSString.functions.hpp"
+#include "NSProcessInfo.functions.hpp"
